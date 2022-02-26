fb07489f479aa4b4da82066f8426183556a6ed24
jbxxnn <87732501+jbxxnn@users.noreply.github.com>
2022-02-26 01:19:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,25 +20,19 @@ service1.addEventListener("mouseover", ClassAddService1, true);
 service1.addEventListener("mouseleave", ClassRemoveService1, true);
 
 function ClassAddService1() {
-    section2.classList.add('service-1-section-2-hover');
     idservice1.classList.add('section-h1-style');
     idservice2.classList.add('section-rest-style');
     idservice3.classList.add('section-rest-style');
     idservice4.classList.add('section-rest-style');
     divBlockImg1.classList.remove('div-block-style');
-    serviceH1.classList.add('h1-section-style-2');
-    serviceP1.classList.add('h1-section-style-2');
 }
 
 function ClassRemoveService1() {
-    section2.classList.remove('service-1-section-2-hover');
     idservice1.classList.remove('section-h1-style');
     idservice2.classList.remove('section-rest-style');
     idservice3.classList.remove('section-rest-style');
     idservice4.classList.remove('section-rest-style');
     divBlockImg1.classList.add('div-block-style');
-    serviceH1.classList.remove('h1-section-style-2');
-    serviceP1.classList.remove('h1-section-style-2');
 }
 
 
@@ -47,25 +41,19 @@ service2.addEventListener("mouseleave", ClassRemoveService2, true);
 
 
 function ClassAddService2() {
-    section2.classList.add('service-2-section-2-hover');
     idservice2.classList.add('section-h1-style');
     idservice1.classList.add('section-rest-style');
     idservice3.classList.add('section-rest-style');
     idservice4.classList.add('section-rest-style');
     divBlockImg2.classList.remove('div-block-style');
-    serviceH1.classList.add('h1-section-style-2');
-    serviceP1.classList.add('h1-section-style-2');
 }
 
 function ClassRemoveService2() {
-    section2.classList.remove('service-2-section-2-hover');
     idservice2.classList.remove('section-h1-style');
     idservice1.classList.remove('section-rest-style');
     idservice3.classList.remove('section-rest-style');
     idservice4.classList.remove('section-rest-style');
     divBlockImg2.classList.add('div-block-style');
-    serviceH1.classList.remove('h1-section-style-2');
-    serviceP1.classList.remove('h1-section-style-2');
 }
 
 
@@ -74,25 +62,19 @@ service3.addEventListener("mouseleave", ClassRemoveService3, true);
 
 
 function ClassAddService3() {
-    section2.classList.add('service-3-section-2-hover');
     idservice3.classList.add('section-h1-style');
     idservice1.classList.add('section-rest-style');
     idservice2.classList.add('section-rest-style');
     idservice4.classList.add('section-rest-style');
     divBlockImg3.classList.remove('div-block-style');
-    serviceH1.classList.add('h1-section-style');
-    serviceP1.classList.add('h1-section-style');
 }
 
 function ClassRemoveService3() {
-    section2.classList.remove('service-3-section-2-hover');
     idservice3.classList.remove('section-h1-style');
     idservice1.classList.remove('section-rest-style');
     idservice2.classList.remove('section-rest-style');
     idservice4.classList.remove('section-rest-style');
     divBlockImg3.classList.add('div-block-style');
-    serviceH1.classList.remove('h1-section-style');
-    serviceP1.classList.remove('h1-section-style');
 }
 
 
@@ -101,25 +83,19 @@ service4.addEventListener("mouseleave", ClassRemoveService4, true);
 
 
 function ClassAddService4() {
-    section2.classList.add('service-4-section-2-hover');
     idservice4.classList.add('section-h1-style');
     idservice1.classList.add('section-rest-style');
     idservice2.classList.add('section-rest-style');
     idservice3.classList.add('section-rest-style');
     divBlockImg4.classList.remove('div-block-style');
-    serviceH1.classList.add('h1-section-style');
-    serviceP1.classList.add('h1-section-style');
 }
 
 function ClassRemoveService4() {
-    section2.classList.remove('service-4-section-2-hover');
     idservice4.classList.remove('section-h1-style');
     idservice1.classList.remove('section-rest-style');
     idservice2.classList.remove('section-rest-style');
     idservice3.classList.remove('section-rest-style');
     divBlockImg4.classList.add('div-block-style');
-    serviceH1.classList.remove('h1-section-style');
-    serviceP1.classList.remove('h1-section-style');
 }
 
 
@@ -133,192 +109,207 @@ const workThree = document.querySelector('#work-three')
 const workFour = document.querySelector('#work-four')
 const workFive = document.querySelector('#work-five')
 const damselInDior = document.querySelector('.damsel-in-dior')
-const damselInDior2 = document.querySelector('.damsel-in-dior2')
 const sosFood = document.querySelector('.sos-food')
-const sosFood2 = document.querySelector('.sos-food2')
 const caseFurniture = document.querySelector('.case-furniture')
-const caseFurniture2 = document.querySelector('.case-furniture2')
 const dryFarmWines = document.querySelector('.dry-farm-wines')
-const dryFarmWines2 = document.querySelector('.dry-farm-wines2')
 const reserveBar = document.querySelector('.reserve-bar')
-const reserveBar2 = document.querySelector('.reserve-bar2')
 const workOneTextH1 = document.querySelector('.work-one-text-h1')
+const workTwoTextH1 = document.querySelector('.work-two-text-h1')
 const workThreeTextH1 = document.querySelector('.work-three-text-h1')
 const workFourTextH1 = document.querySelector('.work-four-text-h1')
 const workFiveTextH1 = document.querySelector('.work-five-text-h1')
-const workOneTextP = document.querySelector('.work-one-text-p')
-const workOneTextPDelivery = document.querySelector('.work-one-text-deliverables-p')
-const workOneTextSpanDelivery = document.querySelector('.work-one-text-deliverables-span')
-const workOneTextPDelivery2 = document.querySelector('.work-one-text-deliverables-p2')
-const workOneTextSpanDelivery2 = document.querySelector('.work-one-text-deliverables-span2')
-const workThreeTextPDelivery = document.querySelector('.work-three-text-deliverables-p')
-const workThreeTextSpanDelivery = document.querySelector('.work-three-text-deliverables-span')
-const workThreeTextPDelivery2 = document.querySelector('.work-three-text-deliverables-p2')
-const workThreeTextSpanDelivery2 = document.querySelector('.work-three-text-deliverables-span2')
-const workFourTextPDelivery = document.querySelector('.work-four-text-deliverables-p')
-const workFourTextSpanDelivery = document.querySelector('.work-four-text-deliverables-span')
-const workFourTextPDelivery2 = document.querySelector('.work-four-text-deliverables-p2')
-const workFourTextSpanDelivery2 = document.querySelector('.work-four-text-deliverables-span2')
-const workFiveTextPDelivery = document.querySelector('.work-five-text-deliverables-p')
-const workFiveTextSpanDelivery = document.querySelector('.work-five-text-deliverables-span')
-const workFiveTextPDelivery2 = document.querySelector('.work-five-text-deliverables-p2')
-const workFiveTextSpanDelivery2 = document.querySelector('.work-five-text-deliverables-span2')
+const gradiantOne = document.querySelector('.gradient-one')
+const gradiantTwo = document.querySelector('.gradient-two')
+const gradiantThree = document.querySelector('.gradient-three')
+const gradiantFour = document.querySelector('.gradient-four')
+const gradiantFive = document.querySelector('.gradient-five')
 
-workOne.addEventListener("mouseover", workMouseOverEffect, true)
-workOne.addEventListener("mouseout", workMouseOutEffect, true)
+workOneTextH1.addEventListener("mouseover", workMouseOverEffect, true)
+workOneTextH1.addEventListener("mouseout", workMouseOutEffect, true)
 
 function workMouseOverEffect() {
     damselInDior.classList.remove('hide-scroll-text');
-    damselInDior2.classList.remove('hide-scroll-text');
+    gradiantOne.classList.remove('hide-scroll-text');
     workOne.classList.add('work-one-style');
-    workTwo.classList.add('other-work-style');
-    workThree.classList.add('other-work-style');
-    workFour.classList.add('other-work-style');
-    workFive.classList.add('other-work-style');
-    workOneTextH1.classList.add('work-one-style-text');
-    workOneTextP.classList.add('work-one-style-text');
-    workOneTextPDelivery.classList.add('work-one-style-text');
-    workOneTextSpanDelivery.classList.add('work-one-style-text');
-    workOneTextPDelivery2.classList.add('work-one-style-text');
-    workOneTextSpanDelivery2.classList.add('work-one-style-text');
+    workOneTextH1.classList.add('work-one-style');
+    work.classList.add('work-1-background');
 }
 
 function workMouseOutEffect() {
     damselInDior.classList.add('hide-scroll-text');
-    damselInDior2.classList.add('hide-scroll-text');
+    gradiantOne.classList.add('hide-scroll-text');
     workOne.classList.remove('work-one-style');
-    workTwo.classList.remove('other-work-style');
-    workThree.classList.remove('other-work-style');
-    workFour.classList.remove('other-work-style');
-    workFive.classList.remove('other-work-style');
-    workOneTextH1.classList.remove('work-one-style-text');
-    workOneTextP.classList.remove('work-one-style-text');
-    workOneTextPDelivery.classList.remove('work-one-style-text');
-    workOneTextSpanDelivery.classList.remove('work-one-style-text');
-    workOneTextPDelivery2.classList.remove('work-one-style-text');
-    workOneTextSpanDelivery2.classList.remove('work-one-style-text');
+    workOneTextH1.classList.remove('work-one-style');
+    work.classList.remove('work-1-background');
 }
 
 
-workTwo.addEventListener("mouseover", workMouse2OverEffect, true)
-workTwo.addEventListener("mouseout", workMouse2OutEffect, true)
+workTwoTextH1.addEventListener("mouseover", workMouse2OverEffect, true)
+workTwoTextH1.addEventListener("mouseout", workMouse2OutEffect, true)
 
 
 function workMouse2OverEffect() {
     sosFood.classList.remove('hide-scroll-text');
-    sosFood2.classList.remove('hide-scroll-text');
+    gradiantTwo.classList.remove('hide-scroll-text');
     workTwo.classList.add('work-two-style');
-    workOne.classList.add('other-work-style');
-    workThree.classList.add('other-work-style');
-    workFour.classList.add('other-work-style');
-    workFive.classList.add('other-work-style');
-    
+    workTwoTextH1.classList.add('work-one-style');
+    work.classList.add('work-2-background');
 }
 function workMouse2OutEffect() {
     sosFood.classList.add('hide-scroll-text');
-    sosFood2.classList.add('hide-scroll-text');
+    gradiantTwo.classList.add('hide-scroll-text');
     workTwo.classList.remove('work-two-style');
-    workOne.classList.remove('other-work-style');
-    workThree.classList.remove('other-work-style');
-    workFour.classList.remove('other-work-style');
-    workFive.classList.remove('other-work-style');
+    workTwoTextH1.classList.remove('work-one-style');
+    work.classList.remove('work-2-background');
 }
 
 
-workThree.addEventListener("mouseover", workMouse3OverEffect, true)
-workThree.addEventListener("mouseout", workMouse3OutEffect, true)
+workThreeTextH1.addEventListener("mouseover", workMouse3OverEffect, true)
+workThreeTextH1.addEventListener("mouseout", workMouse3OutEffect, true)
 
 function workMouse3OverEffect() {
     caseFurniture.classList.remove('hide-scroll-text');
-    caseFurniture2.classList.remove('hide-scroll-text');
+    gradiantThree.classList.remove('hide-scroll-text');
     workThree.classList.add('work-three-style');
-    workOne.classList.add('other-work-style');
-    workTwo.classList.add('other-work-style');
-    workFour.classList.add('other-work-style');
-    workFive.classList.add('other-work-style');
-    workThreeTextH1.classList.add('work-one-style-text');
-    workThreeTextPDelivery.classList.add('work-one-style-text');
-    workThreeTextSpanDelivery.classList.add('work-one-style-text');
-    workThreeTextPDelivery2.classList.add('work-one-style-text');
-    workThreeTextSpanDelivery2.classList.add('work-one-style-text');
+    workThreeTextH1.classList.add('work-one-style');
+    work.classList.add('work-3-background');
 }
 function workMouse3OutEffect() {
     caseFurniture.classList.add('hide-scroll-text');
-    caseFurniture2.classList.add('hide-scroll-text');
+    gradiantThree.classList.add('hide-scroll-text');
     workThree.classList.remove('work-three-style');
-    workOne.classList.remove('other-work-style');
-    workTwo.classList.remove('other-work-style');
-    workFour.classList.remove('other-work-style');
-    workFive.classList.remove('other-work-style');
-    workThreeTextH1.classList.remove('work-one-style-text');
-    workThreeTextPDelivery.classList.remove('work-one-style-text');
-    workThreeTextSpanDelivery.classList.remove('work-one-style-text');
-    workThreeTextPDelivery2.classList.remove('work-one-style-text');
-    workThreeTextSpanDelivery2.classList.remove('work-one-style-text');
+    workThreeTextH1.classList.remove('work-one-style');
+    work.classList.remove('work-3-background');
 }
 
 
-workFour.addEventListener("mouseover", workMouse4OverEffect, true)
-workFour.addEventListener("mouseout", workMouse4OutEffect, true)
+workFourTextH1.addEventListener("mouseover", workMouse4OverEffect, true)
+workFourTextH1.addEventListener("mouseout", workMouse4OutEffect, true)
 
 function workMouse4OverEffect() {
     dryFarmWines.classList.remove('hide-scroll-text');
-    dryFarmWines2.classList.remove('hide-scroll-text');
+    gradiantFour.classList.remove('hide-scroll-text');
     workFour.classList.add('work-four-style');
-    workOne.classList.add('other-work-style');
-    workTwo.classList.add('other-work-style');
-    workThree.classList.add('other-work-style');
-    workFive.classList.add('other-work-style');
-    workFourTextH1.classList.add('work-one-style-text');
-    workFourTextPDelivery.classList.add('work-one-style-text');
-    workFourTextSpanDelivery.classList.add('work-one-style-text');
-    workFourTextPDelivery2.classList.add('work-one-style-text');
-    workFourTextSpanDelivery2.classList.add('work-one-style-text');
+    workFourTextH1.classList.add('work-one-style');
+    work.classList.add('work-4-background');
 }
 function workMouse4OutEffect() {
     dryFarmWines.classList.add('hide-scroll-text');
-    dryFarmWines2.classList.add('hide-scroll-text');
+    gradiantFour.classList.add('hide-scroll-text');
     workFour.classList.remove('work-four-style');
-    workOne.classList.remove('other-work-style');
-    workTwo.classList.remove('other-work-style');
-    workThree.classList.remove('other-work-style');
-    workFive.classList.remove('other-work-style');
-    workFourTextH1.classList.remove('work-one-style-text');
-    workFourTextPDelivery.classList.remove('work-one-style-text');
-    workFourTextSpanDelivery.classList.remove('work-one-style-text');
-    workFourTextPDelivery2.classList.remove('work-one-style-text');
-    workFourTextSpanDelivery2.classList.remove('work-one-style-text');
+    workFourTextH1.classList.remove('work-one-style');
+    work.classList.remove('work-4-background');
 }
 
 
-workFive.addEventListener("mouseover", workMouse5OverEffect, true)
-workFive.addEventListener("mouseout", workMouse5OutEffect, true)
+workFiveTextH1.addEventListener("mouseover", workMouse5OverEffect, true)
+workFiveTextH1.addEventListener("mouseout", workMouse5OutEffect, true)
 
 function workMouse5OverEffect() {
     reserveBar.classList.remove('hide-scroll-text');
-    reserveBar2.classList.remove('hide-scroll-text');
+    gradiantFive.classList.remove('hide-scroll-text');
     workFive.classList.add('work-five-style');
-    workOne.classList.add('other-work-style');
-    workTwo.classList.add('other-work-style');
-    workThree.classList.add('other-work-style');
-    workFour.classList.add('other-work-style');
-    workFiveTextH1.classList.add('work-one-style-text');
-    workFiveTextPDelivery.classList.add('work-one-style-text');
-    workFiveTextSpanDelivery.classList.add('work-one-style-text');
-    workFiveTextPDelivery2.classList.add('work-one-style-text');
-    workFiveTextSpanDelivery2.classList.add('work-one-style-text');
+    workFiveTextH1.classList.add('work-one-style');
+    work.classList.add('work-5-background');
 }
 function workMouse5OutEffect() {
     reserveBar.classList.add('hide-scroll-text');
-    reserveBar2.classList.add('hide-scroll-text');
+    gradiantFive.classList.add('hide-scroll-text');
     workFive.classList.remove('work-five-style');
-    workOne.classList.remove('other-work-style');
-    workTwo.classList.remove('other-work-style');
-    workThree.classList.remove('other-work-style');
-    workFour.classList.remove('other-work-style');
-    workFiveTextH1.classList.remove('work-one-style-text');
-    workFiveTextPDelivery.classList.remove('work-one-style-text');
-    workFiveTextSpanDelivery.classList.remove('work-one-style-text');
-    workFiveTextPDelivery2.classList.remove('work-one-style-text');
-    workFiveTextSpanDelivery2.classList.remove('work-one-style-text');
+    workFiveTextH1.classList.remove('work-one-style');
+    work.classList.remove('work-5-background');
 }
+
+
+
+// Custom Cursor
+
+let clientX = -100;
+let clientY = -100;
+const innerCursor = document.querySelector(".cursor--small");
+
+const initCursor = () => {
+  // add listener to track the current mouse position
+  document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  });
+  const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    
+    
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+
+initCursor();
+
+
+
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCursor;
+
+const initCanvas = () => {
+    const canvas = document.querySelector(".cursor--canvas");
+    const shapeBounds = {
+      width: 75,
+      height: 75
+    };
+    paper.setup(canvas);
+    const strokeColor = "#d90a2c";
+    const strokeWidth = 1;
+    const segments = 8;
+    const radius = 15;
+
+    const polygon = new paper.Path.RegularPolygon(
+        new paper.Point(0, 0),
+        segments,
+        radius
+      );
+
+      polygon.strokeColor = strokeColor;
+  polygon.strokeWidth = strokeWidth;
+  polygon.smooth();
+  group = new paper.Group([polygon]);
+  group.applyMatrix = false;
+
+  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  let bigCoordinates = [];
+
+  const lerp = (a, b, n) => {
+    return (1 - n) * a + n * b;
+  };
+
+  const map = (value, in_min, in_max, out_min, out_max) => {
+    return (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+  };
+
+  paper.view.onFrame = event => {
+    // using linear interpolation, the circle will move 0.2 (20%)
+    // of the distance between its current position and the mouse
+    // coordinates per Frame
+    lastX = lerp(lastX, clientX, 0.1);
+    lastY = lerp(lastY, clientY, 0.05);
+    group.position = new paper.Point(lastX, lastY);
+  }
+
+
+}
+initCanvas();
+
+
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: 0.5,
+  });
+
+
+
+  
